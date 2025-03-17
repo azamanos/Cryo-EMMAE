19ff56e9975282782af74f2d8b8bde306b040089
Andreas Zamanos <andreas.zamanos@athenarc.gr>
2025-03-17 17:35:07 +0200

    minor fix

--- params/params_preprocess.py
+++ params/params_preprocess.py
@@ -6,7 +6,7 @@ def get_args():
 
     # Preprocess Parameters
     parser.add_argument('--md', type=str, default=None, required=True,help='Directory with micrograph to be processed')
-    parser.add_argument('--ml', type=list, default=None, help='List of micrographs to be processed')
+    parser.add_argument('--ml', type=str, default=None, help='List of micrographs to be processed in npy format')
     parser.add_argument('--t', type=bool, default=True, help='If data type is mrc or image type file, default is True == mrc type files.')
     parser.add_argument('--rs', type=int, default=1024, help='Resize shape')
     parser.add_argument('--pd', type=int, default=200, required=False,help='Particle diameter in pixels for the original shape')
